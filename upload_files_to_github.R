310710fef51b3c78edf9317bdc3576444609955e
jhammondusgs <jhammond@usgs.gov>
2023-02-14 22:06:54 -0500

    updated git code with notes

--- upload_files_to_github.R
+++ upload_files_to_github.R
@@ -1,21 +1,11 @@
 # how to push to github using command line
 # https://happygitwithr.com/push-pull-github.html#push-pull-github
 
-git config --global user.name 'jhammondusgs'
-git config --global user.email 'jhammond@usgs.gov'
-git config --global credential.helper osxkeychain
-git clone https://github.com/jhammondusgs/powell_streamflow_depletion_regional_analysis
-git add 8_development_of_a_gage_classification_scheme.R # to add a file to track
-
-1)
-
-cd powell_streamflow_depletion_regional_analysis
-
-2)
+# cd powell_streamflow_depletion_regional_analysis
+# git add ...
 
 git status
 
-3)
+git commit -m "updated code to ..."
 
-git commit -m "updated code to work with files stored on google drive"
-
+git push
